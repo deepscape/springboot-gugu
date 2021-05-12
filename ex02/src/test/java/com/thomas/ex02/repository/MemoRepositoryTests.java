90bda8c8aac5a3e5ffb4ca8a36927c9f7bccb10d
deepscape <hdkim.scala@gmail.com>
2021-05-12 21:25:12 +0900

    chapter 02. paging and sorting

--- ex02/src/test/java/com/thomas/ex02/repository/MemoRepositoryTests.java
+++ ex02/src/test/java/com/thomas/ex02/repository/MemoRepositoryTests.java
@@ -4,6 +4,10 @@ import com.thomas.ex02.entity.Memo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import javax.transaction.Transactional;
 import java.util.NoSuchElementException;
@@ -83,4 +87,40 @@ public class MemoRepositoryTests {
         Long mno = 101L;
         memoRepository.deleteById(mno);
     }
+
+    @Test
+    public void testPageDefault() {
+        // 1페이지 10개
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<Memo> result = memoRepository.findAll(pageable);
+
+        System.out.println(result);
+        System.out.println("=================================");
+        System.out.println("Total Pages: " + result.getTotalPages());       // 총 페이지 개수
+        System.out.println("Total Count: " + result.getTotalElements());    // 전체 개수
+        System.out.println("Page Number: " + result.getNumber());           // 현재 페이지 번호
+        System.out.println("Page Size: " + result.getSize());               // 페이지당 데이터 개수
+        System.out.println("has next page? : " + result.hasNext());         // 다음 페이지 존재 여부
+        System.out.println("first page? : " + result.isFirst());             // 시작 페이지(0) 여부
+        System.out.println("=================================");
+
+        for (Memo memo : result.getContent()) {
+            System.out.println(memo);
+        }
+    }
+
+    @Test
+    public void testSort() {
+        Sort sort1 = Sort.by("mno").descending();
+        Sort sort2 = Sort.by("memoText").ascending();
+        Sort sortAll = sort1.and(sort2);
+
+        Pageable pageable = PageRequest.of(0, 10, sortAll);
+        Page<Memo> result = memoRepository.findAll(pageable);
+
+        result.get().forEach(memo -> {
+            System.out.println(memo);
+        });
+    }
+
 }
