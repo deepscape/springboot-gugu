de6a49a7ce7b39c1fb889e94466dd76a066a85eb
deepscape <hdkim.scala@gmail.com>
2021-05-13 21:54:00 +0900

    chapter 02. query method

--- ex02/src/main/java/com/thomas/ex02/repository/MemoRepository.java
+++ ex02/src/main/java/com/thomas/ex02/repository/MemoRepository.java
@@ -1,7 +1,21 @@
 package com.thomas.ex02.repository;
 
 import com.thomas.ex02.entity.Memo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemoRepository extends JpaRepository<Memo, Long> {
+
+    // Query Method : Memo 객체의 mno 값이 170부터 180 사이의 객체를 구하고, mno 의 역순으로 정렬
+    List<Memo> findByMnoBetweenOrderByMnoDesc(Long from, Long to);
+
+    // Pageable 을 통해 정렬 조건을 조절 -> 좀 더 단순한 형태의 메소드 선언
+    Page<Memo> findByMnoBetween(Long from, Long to, Pageable pageable);
+
+    // Delete Query Method : 메모의 번호가 10보다 작은 데이터를 삭제
+    void deleteMemoByMnoLessThan(Long num);
+
 }
--- ex02/src/test/java/com/thomas/ex02/repository/MemoRepositoryTests.java
+++ ex02/src/test/java/com/thomas/ex02/repository/MemoRepositoryTests.java
@@ -8,8 +8,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.test.annotation.Commit;
 
 import javax.transaction.Transactional;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.stream.IntStream;
@@ -123,4 +125,27 @@ public class MemoRepositoryTests {
         });
     }
 
+    @Test
+    public void testQueryMethod() {
+        List<Memo> list = memoRepository.findByMnoBetweenOrderByMnoDesc(170L, 180L);
+
+        for (Memo memo : list) {
+            System.out.println(memo);
+        }
+    }
+
+    @Test
+    public void testQueryMethodWithPageable() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("mno").descending());
+        Page<Memo> result = memoRepository.findByMnoBetween(110L, 120L, pageable);
+
+        result.get().forEach(memo -> System.out.println(memo));
+    }
+
+    @Commit
+    @Transactional
+    @Test
+    public void testDeleteQueryMethod() {
+        memoRepository.deleteMemoByMnoLessThan(110L);
+    }
 }
