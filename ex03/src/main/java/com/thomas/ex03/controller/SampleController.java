b6b6836b2e26162e2730b8632b12cb0f4c08d360
deepscape <hdkim.scala@gmail.com>
2021-05-14 18:06:30 +0900

    chapter 03. thymeleaf

---
+++ ex03/src/main/java/com/thomas/ex03/controller/SampleController.java
@@ -0,0 +1,40 @@
+package com.thomas.ex03.controller;
+
+import com.thomas.ex03.dto.SampleDTO;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Controller
+@RequestMapping("/sample")
+@Log4j2
+public class SampleController {
+
+    @GetMapping("/ex1")
+    public void ex1() {
+        log.info("ex1................");
+    }
+
+    // {} 를 사용하면 하나 이상의 URL 을 지정할 수 있다.
+    @GetMapping({"/ex2"})
+    public void exModel(Model model) {
+        List<SampleDTO> list = IntStream.rangeClosed(1,20).asLongStream().mapToObj(i -> {
+            SampleDTO dto = SampleDTO.builder().sno(i)
+                                               .first("First.."+i)
+                                               .last("Last.."+i)
+                                               .regTime(LocalDateTime.now())
+                                               .build();
+            return dto;
+        }).collect(Collectors.toList());
+
+        model.addAttribute("list", list);
+    }
+
+}
