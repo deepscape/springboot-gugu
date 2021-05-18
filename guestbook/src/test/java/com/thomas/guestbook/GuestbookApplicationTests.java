e2939374ec2e6a6b9b4a795ad22362edf0fb9f0e
deepscape <hdkim.scala@gmail.com>
2021-05-18 13:23:26 +0900

    chapter 04. start

---
+++ guestbook/src/test/java/com/thomas/guestbook/GuestbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thomas.guestbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
