8182d8ba85adb1a0a4bc49517a5ae6ea0fff049b
deepscape <hdkim.scala@gmail.com>
2021-05-19 19:01:59 +0900

    chapter 04. Querydsl setting

--- guestbook/src/main/java/com/thomas/guestbook/repository/GuestbookRepository.java
+++ guestbook/src/main/java/com/thomas/guestbook/repository/GuestbookRepository.java
@@ -2,6 +2,8 @@ package com.thomas.guestbook.repository;
 
 import com.thomas.guestbook.entity.Guestbook;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface GuestbookRepository extends JpaRepository<Guestbook, Long> {
+// Querydsl 사용하게 되면, QuerydslPredicateExecutor 추가 상속
+public interface GuestbookRepository extends JpaRepository<Guestbook, Long>, QuerydslPredicateExecutor<Guestbook> {
 }
