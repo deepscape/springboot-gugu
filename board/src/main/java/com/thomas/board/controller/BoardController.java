305f1329f9282426c72c099f6add56f87fb24610
deepscape <hdkim.scala@gmail.com>
2021-06-02 15:46:27 +0900

    chapter 05. 화면 처리 - 게시물 조회

--- board/src/main/java/com/thomas/board/controller/BoardController.java
+++ board/src/main/java/com/thomas/board/controller/BoardController.java
@@ -8,6 +8,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -42,6 +43,22 @@ public class BoardController {
         redirectAttributes.addFlashAttribute("msg", bno);
 
         return "redirect:/board/list";
+    }
+
+    /* @ModelAttribute
+        - 파라미터로 넘겨 준 타입의 오브젝트를 자동 생성
+        - 생성된 오브젝트에 http 로 넘어 온 값들을 자동으로 바인딩
+        - @ModelAttribute 어노테이션이 붙은 객체가 자동으로 Model 객체에 추가되고 뷰단으로 전달
+     */
+    @GetMapping("/read")
+    public void read(@ModelAttribute("requestDTO") PageRequestDTO pageRequestDTO, Long bno, Model model) {
+        log.info("bno: " + bno);
+
+        BoardDTO boardDTO = boardService.get(bno);
+
+        log.info(boardDTO);
+
+        model.addAttribute("dto", boardDTO);
     }
 
 }
