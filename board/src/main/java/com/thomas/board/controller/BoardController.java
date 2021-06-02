f99ba56dfebd55ad0a22cb74f1bcb776482bdaa3
deepscape <hdkim.scala@gmail.com>
2021-06-02 15:03:03 +0900

    chapter 05. 화면 처리 - 게시물 등록

--- board/src/main/java/com/thomas/board/controller/BoardController.java
+++ board/src/main/java/com/thomas/board/controller/BoardController.java
@@ -1,5 +1,6 @@
 package com.thomas.board.controller;
 
+import com.thomas.board.dto.BoardDTO;
 import com.thomas.board.dto.PageRequestDTO;
 import com.thomas.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/board/")
@@ -22,4 +25,23 @@ public class BoardController {
         log.info("list..............." + pageRequestDTO);
         model.addAttribute("result", boardService.getList(pageRequestDTO));
     }
+
+    @GetMapping("/register")
+    public void register() {
+        log.info("register get...");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(BoardDTO dto, RedirectAttributes redirectAttributes) {
+        log.info("dto..." + dto);
+
+        // 새로 추가된 엔티티 번호
+        Long bno = boardService.register(dto);
+        log.info("bno: " + bno);
+
+        redirectAttributes.addFlashAttribute("msg", bno);
+
+        return "redirect:/board/list";
+    }
+
 }
