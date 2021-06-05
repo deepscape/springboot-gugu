95b9a7b5eae2e71c517b55fd69bb8cebabb9e7c5
deepscape <hdkim.scala@gmail.com>
2021-06-05 20:20:25 +0900

    chapter 07. N+1 problem

--- mreview/src/main/java/com/thomas/mreview/repository/MovieRepository.java
+++ mreview/src/main/java/com/thomas/mreview/repository/MovieRepository.java
@@ -1,7 +1,18 @@
 package com.thomas.mreview.repository;
 
 import com.thomas.mreview.entity.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    // 영화, 영화 이미지, 평점 평균, 리뷰 수
+    // max(mi) 의 경우, N+1 문제를 발생시키므로 절대 유의할 것
+    @Query("select m, mi, avg(coalesce(r.grade, 0)), count(distinct r) from Movie m " +
+            "left join MovieImage mi on mi.movie = m " +
+            "left join Review r on r.movie = m group by m")
+    Page<Object[]> getListPage(Pageable pageable);
+
 }
