3fb8d0da9e6b10a4f642cee0bd9cbed9e4eb8048
deepscape <hdkim.scala@gmail.com>
2021-06-06 19:11:52 +0900

    chapter 07. 특정 영화에 대한 조회

--- mreview/src/main/java/com/thomas/mreview/repository/ReviewRepository.java
+++ mreview/src/main/java/com/thomas/mreview/repository/ReviewRepository.java
@@ -1,7 +1,13 @@
 package com.thomas.mreview.repository;
 
+import com.thomas.mreview.entity.Movie;
 import com.thomas.mreview.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    // 특정 영화의 모든 리뷰와 회원 닉네임
+    List<Review> findByMovie(Movie movie);
 }
