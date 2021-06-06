2d673022692172ad78eefc6e8892d234098d7c8f
deepscape <hdkim.scala@gmail.com>
2021-06-06 20:20:17 +0900

    chapter 07. @EntityGraph / 특정 영화의 모든 리뷰와 회원 닉네임

--- mreview/src/main/java/com/thomas/mreview/repository/ReviewRepository.java
+++ mreview/src/main/java/com/thomas/mreview/repository/ReviewRepository.java
@@ -2,6 +2,7 @@ package com.thomas.mreview.repository;
 
 import com.thomas.mreview.entity.Movie;
 import com.thomas.mreview.entity.Review;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,5 +10,6 @@ import java.util.List;
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     // 특정 영화의 모든 리뷰와 회원 닉네임
+    @EntityGraph(attributePaths = {"member"}, type = EntityGraph.EntityGraphType.FETCH)
     List<Review> findByMovie(Movie movie);
 }
--- mreview/src/test/java/com/thomas/mreview/repository/ReviewRepositoryTests.java
+++ mreview/src/test/java/com/thomas/mreview/repository/ReviewRepositoryTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -34,6 +35,21 @@ public class ReviewRepositoryTests {
                                                  .build();
 
             reviewRepository.save(movieReview);
+        });
+    }
+
+    @Test
+    public void testGetMovieReviews() {
+
+        Movie movie = Movie.builder().mno(91L).build();
+        List<Review> result = reviewRepository.findByMovie(movie);
+
+        result.forEach(movieReview -> {
+            System.out.print(movieReview.getReviewnum());
+            System.out.print("\t" + movieReview.getGrade());
+            System.out.print("\t" + movieReview.getText());
+            System.out.print("\t" + movieReview.getMember().getEmail());
+            System.out.println("-------------------------------------");
         });
     }
 }
