a581ddf7f02170a67fd0cc726be713780e89920d
deepscape <hdkim.scala@gmail.com>
2021-06-16 20:05:11 +0900

    chapter 10. AuthenticationManager 설정

--- club/src/main/java/com/thomas/club/config/SecurityConfig.java
+++ club/src/main/java/com/thomas/club/config/SecurityConfig.java
@@ -1,10 +1,31 @@
 package com.thomas.club.config;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @Log4j2
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // 패스워드 암호화
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        // 사용자 계정은 user1
+        auth.inMemoryAuthentication().withUser("user1")
+        // 1111 패스워드 인코딩 결과 - 테스트 코드 참고
+        .password("$2a$10$gXklK.tDOEbOtE5zX7.mTuykYZKnPauAVGZcPP0sqU.ch1ACXIx3S")
+        .roles("USER");
+
+    }
 }
