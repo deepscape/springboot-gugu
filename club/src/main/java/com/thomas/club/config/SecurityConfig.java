7eb9392e650675ea6cd2520b351d8f2644417b47
deepscape <hdkim.scala@gmail.com>
2021-06-20 20:40:25 +0900

    chapter 11. Redirect Strategy - social login 은 회원 정보를 수정하는 경로로 이동

--- club/src/main/java/com/thomas/club/config/SecurityConfig.java
+++ club/src/main/java/com/thomas/club/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public ClubLoginSuccessHandler successHandler() {
-        return new ClubLoginSuccessHandler();
+        return new ClubLoginSuccessHandler(passwordEncoder());
     }
 
 
